da0dbf6eb8c87af514c0f266d7b2b9538fd19080
Ata Can Çalışkan <caliskanataa@gmail.com>
2023-10-06 20:54:03 +0300

    Initial Commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Ata Çalışkan on 6.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
